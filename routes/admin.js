209ea080798d3d754352115d0f09a651eb1bb26d
syedmujtabanaqvi <nushat786syed@gmail.com>
2026-05-21 08:17:05 +0500

    add: Add API in Admin Portal for Total Orders

--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,19 @@ const cors = require("cors");
 const sql = require("mssql");
 const config = require('../db');
 
+// TOTAL ORDER IN ADMIN PORTL
+router.get("/TotalOrders", async (req, res) => {
 
+    try {
+
+        let pool = await sql.connect(config)
+
+        let result = await pool.request().query("SELECT COUNT(*) AS TotalOrders FROM MenuItems");
+
+        res.json(result.recordset[0].TotalOrders);
+    }
+    catch (err) {
+        res.send(error.message)
+    }
+});
  module.exports = router;
